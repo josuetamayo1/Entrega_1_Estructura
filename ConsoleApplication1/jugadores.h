bbe1e07ae868ac8335e8110647143332768cc481
josuetamayo1 <josuetamayour@gmail.com>
2026-09-16 22:41:01 -0500

    clase jugadores

---
+++ ConsoleApplication1/jugadores.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include "carta.h"
+
+class Jugadores {
+private:
+	int id;
+	std::vector<Carta*> mazo;
+	int puntaje;
+
+public:
+	Jugadores(int _id, std::vector<Carta*> _mazo, int _puntaje) {
+		id = _id;
+		mazo = _mazo;
+		puntaje = _puntaje;
+	}
+	Carta* jugarCarta(Carta* carta) {
+
+	}
+	void ganarCartas() {
+
+	}
+	void perderCartas() {
+
+	}
+	char elegirJuego() {
+
+	}
+};s
