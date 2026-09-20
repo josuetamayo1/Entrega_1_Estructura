ab84b0b36e18432fe1ce6eb8cd73ae53a8642c3e
josuetamayo1 <josuetamayour@gmail.com>
2026-09-20 12:24:37 -0500

    clase jugadores

--- ConsoleApplication1/jugadores.h
+++ ConsoleApplication1/jugadores.h
@@ -16,15 +16,19 @@ public:
 		puntaje = _puntaje;
 	}
 	Carta* jugarCarta(Carta* carta) {
+		// Lógica para que el jugador juegue una carta de su mazo
 
 	}
 	void ganarCartas() {
+		// Lógica para cuando el jugador gana la ronda y recibe cartas o puntos
 
 	}
 	void perderCartas() {
+		// Lógica para cuando el jugador pierde cartas tras perder la ronda
 
 	}
 	char elegirJuego() {
+		// Lógica para que el jugador elija el color/tipo de juego a jugar
 
 	}
-};s
+};
